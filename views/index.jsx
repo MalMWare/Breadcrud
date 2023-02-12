7a643e03a378c4d5bcbec6cb3e9b78325cc55895
MalMWare <malkelleymartin@gmail.com>
2023-02-12 11:43:41 -0800

    created new route page with the ability to add new bread

--- views/index.jsx
+++ views/index.jsx
@@ -7,22 +7,23 @@ function Index ({breads, title}) {
   <h2>Index Page</h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
-   <ul>
-    {
-        
-        breads.map((bread, index) => {
-            return (
-                <li key={index}>
-                  <a href={`/breads/${index}`}>
-                    {bread.name}
-                  </a>
-                </li>
-            )
-        })
-    }
-          
-  </ul> 
-</Default>
+  <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key= {index}>
+              <a href={`/breads/${index}`}>
+                {bread.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className='newButton'>
+        <a href='/breads/new'>
+          <button>Add a new bread</button>
+        </a>
+      </div>
+    </Default>
     )
 }
 
