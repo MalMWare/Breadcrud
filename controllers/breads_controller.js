21e70879001108025ab85c80014aa9106c9b5e7c
MalMWare <malkelleymartin@gmail.com>
2023-02-18 22:42:31 -0800

    created delete and edit routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,15 +16,31 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
       res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
       })
     } else {
       res.send('404')
     }
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 // CREATE
@@ -40,5 +56,18 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 })
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+
 
 module.exports = breads
