c22f480a5b5e9127b0c84c252cdc6fa6b7cfb6fa
MalMWare <malkelleymartin@gmail.com>
2023-02-23 20:34:49 -0800

    updated seed data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -72,35 +72,38 @@ breads.delete('/:id', (req, res) => {
 
 //hard data
 breads.get('/data/seed', (req, res) => {
-  Bread.insertMany([
-    {
-      name: 'Rye',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Monica',
-    },
-    {
-      name: 'French',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-      baker: 'Joey',
-    },
-    {
-      name: 'Gluten Free',
-      hasGluten: false,
-      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Rachel',
-    },
-    {
-      name: 'Pumpernickel',
-      hasGluten: true,
-      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-      baker: 'Ross',
-    }
-  ])
-    .then(createdBreads => {
-      res.redirect('/breads')
-    })
+  Baker.findOne({name: 'Joey'}).then(baker => {
+    Bread.insertMany([
+      {
+        name: 'Rye',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        baker: baker,
+      },
+      {
+        name: 'French',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+        baker: baker,
+      },
+      {
+        name: 'Gluten Free',
+        hasGluten: false,
+        image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        baker: baker,
+      },
+      {
+        name: 'Pumpernickel',
+        hasGluten: true,
+        image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        baker: baker,
+      }
+    ])
+      .then(createdBreads => {
+        res.redirect('/breads')
+      })
+  })
+  
 })
 
 // CREATE
